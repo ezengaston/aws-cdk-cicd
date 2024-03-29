1b45a249d79a6ae03f0ac5aa7b22c9fd801799a5
Ezen <82026690+ezengaston@users.noreply.github.com>
2024-03-29 15:08:24 +0000

    Added visitor count

--- lambda/main.py
+++ lambda/main.py
@@ -1,9 +1,27 @@
 import os
+import boto3
 
 def handler(event, context):
+    path = event["rawPath"]
+    if path != "/":
+        return {"statusCode": 404, "body": "Not found."}
+
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(os.environ.get("TABLE_NAME" ))
+
+    response = table.get_item(Key={"key": "visit_count"})
+    if "Item" in response:
+        visit_count = response["Item"]["value"]
+    else:
+        visit_count = 0
+
+    new_visit_count = visit_count + 1
+    table.put_item(Key={"key": "visit_count", "value": new_visit_count})
+
     version = os.environ.get("VERSION", "0.0")
     response_body = {
         "message": "Hello World 👋",
-        "version": version
+        "version": version,
+        "visit_count": new_visit_count
     }
     return {"statusCode": 200, "body": response_body}
